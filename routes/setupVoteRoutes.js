f2429feac262507ee969175dc6c7ba0b0d895023
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-07-28 16:02:13 +0000

    setup controller

--- routes/setupVoteRoutes.js
+++ routes/setupVoteRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {IsAuth} from '../middleware/isAuth';
-import { GetSetupVote, PostVoteSetup , postPinGeneration, GetGeneratedPin, ViewVote, EditVoteModel} from '../controller/setupVote';
+import SetupController from '../controller/setupVote';
 
 const auth = new IsAuth();
 
@@ -14,26 +14,26 @@ const router = express.Router();
  */
 
 //<<<< GET request to /api/setupVote 
-router.get('/setupVote',auth.verifyAuth, new GetSetupVote().getFirstPage);
+//router.get('/setupVote',auth.verifyAuth, new GetSetupVote().getFirstPage);
 
 // <<<<< POST request to the api/setupVote to generate the voting environment
-router.post('/setupVote', auth.verifyAuth, new PostVoteSetup().postVote);
+router.post('/setupVote', auth.verifyAuth, SetupController.voteConfig);
 
 // <<<<< POST request to api/setpin to generate a voting pin for a particular user to vote with for authenticated user
-router.post('/setpin', auth.verifyAuth, new postPinGeneration().setPin);
+//router.post('/setpin', auth.verifyAuth, new postPinGeneration().setPin);
 
 /** <<<<< GET request to api/getpins to view the list of generated pins 
     << for authenticated user >>
 */ 
-router.get('/getpins', auth.verifyAuth, new GetGeneratedPin().getpins);
+//router.get('/getpins', auth.verifyAuth, new GetGeneratedPin().getpins);
 
 /**
  * GET requesrt to api/viewSetups to view the voting platform created
  * <<< for authenticated user only
  */
-router.get('/viewSetups', auth.verifyAuth, new ViewVote().view );
+//router.get('/viewSetups', auth.verifyAuth, new ViewVote().view );
 
-router.post('/edit', auth.verifyAuth, new EditVoteModel().editVote )
+//router.post('/edit', auth.verifyAuth, new EditVoteModel().editVote )
 
 export default router;
 
